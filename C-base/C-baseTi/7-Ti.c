d00fb0db2a0c38376698d217c4f96d2af971c8cb
Yu-1120 <1186893249@qq.com>
2022-08-19 15:23:31 +0800

    update-8-19

--- C-base/C-baseTi/7-Ti.c
+++ C-base/C-baseTi/7-Ti.c
@@ -6,12 +6,15 @@
 int main() {
 
     char *a[] = {"It's", "C Plus", "wecat"};
+    //指针的数组的，就是每个元素是char*
 
     char **pa = a;
+    //*pa的*告诉我们它是指针，指向的类型是char*
+
 
     pa++;
-
-    printf("%s\n", *pa);
+    //指向第二个是‘a’的地址
+    printf("%s\n", *pa);//"at"
 
     return 0;
 
